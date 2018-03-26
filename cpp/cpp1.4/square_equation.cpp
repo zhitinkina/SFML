c542c826bd377a85c2ab3d1a77d57d533e1a8e09
Anna Cergeevna <admin@anya>
2018-03-25 23:26:29 -0800

    Правка замечаний

--- cpp/cpp1.4/square_equation.cpp
+++ cpp/cpp1.4/square_equation.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     // 1. prompt user to enter coefficients
-    std::puts("please enter a and b for 'ax^2+bx+c=0':");
+    std::puts("Please enter a, b and c:");
 
     // 2. read coefficients for equation `ax^2+bx+c=0`
     float a = 0;
@@ -14,28 +14,28 @@ int main()
 
     if (a == 0)
     {
-        std::printf("kornei net!");
+        std::printf("There r no roots!");
     }
     else
     {
-        float Deskriminant = b * b - 4 * a * c;
-        if (Deskriminant < 0)
+        float deskriminant = b * b - 4 * a * c;
+        if (deskriminant < 0)
         {
-            std::puts("kornei net!");
+            std::printf("There r no roots!");
         }
-        if (Deskriminant == 0)
+        if (deskriminant == 0)
         {
             float x = -b / c;
-            std::printf("answer: %.2f\n", x);
+            std::printf("x: %.2f\n", x);
         }
-        if (Deskriminant > 0)
+        if (deskriminant > 0)
         {
-            Deskriminant = sqrt(Deskriminant);
-            float x1 = (-b + Deskriminant) / (2 * a);
-            float x2 = (-b - Deskriminant) / (2 * a);
+            deskriminant = std::sqrt(deskriminant);
+            float x1 = (-b + deskriminant) / (2 * a);
+            float x2 = (-b - deskriminant) / (2 * a);
 
-            std::printf("answer1: %.2f\n", x1);
-            std::printf("answer2: %.2f\n", x2);
+            std::printf("x1: %.2f\n", x1);
+            std::printf("x2: %.2f\n", x2);
         }
     }
 }
